261f08fb1507d8e543aa60d8cdb401941468aa34
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-04-05 13:30:00 +0530

    Add date-fns library for handling days according to calendar arithmatic

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const labelDay = document.querySelector('.label.day');
-const labelMonth = document.querySelector('.label.month');
-const labelYear = document.querySelector('.label.year');
+const validationYearEmpty = document.querySelector('.year-validation-empty');
+const validationYear = document.querySelector('.year-validation');
 
-const inputDay = document.querySelector('#day');
-const inputMonth = document.querySelector('#month');
-const inputYear = document.querySelector('#year');
+const validationMonthEmpty = document.querySelector('.month-validation-empty');
+const validationMonth = document.querySelector('.month-validation');
 
-const displayDay = document.querySelector('.days > span');
+const validationDayEmpty = document.querySelector('.day-validation-empty');
+const validationDay = document.querySelector('.day-validation');
+
 const displayMonth = document.querySelector('.months > span');
 const displayYear = document.querySelector('.years > span');
+const displayDay = document.querySelector('.days > span');
 
-const validationDay = document.querySelector('.day-validation');
-const validationDayEmpty = document.querySelector('.day-validation-empty');
+const labelMonth = document.querySelector('.label.month');
+const labelYear = document.querySelector('.label.year');
+const labelDay = document.querySelector('.label.day');
 
-const validationMonth = document.querySelector('.month-validation');
-const validationMonthEmpty = document.querySelector('.month-validation-empty');
-
-const validationYear = document.querySelector('.year-validation');
-const validationYearEmpty = document.querySelector('.year-validation-empty');
+const inputMonth = document.querySelector('#month');
+const inputYear = document.querySelector('#year');
+const inputDay = document.querySelector('#day');
 
 const dobValidation = document.querySelector('.dob-validation');
-
 const form = document.querySelector('.age-form');
 
 function updateInput(label, input, validationOne, validationTwo, maxLength) {
@@ -31,37 +30,6 @@ function updateInput(label, input, validationOne, validationTwo, maxLength) {
   if (input.value.length > maxLength)
     input.value = input.value.slice(0, maxLength);
   validationTwo.classList.add('d-none');
-}
-
-function calculateAge(birthDate) {
-  const today = new Date();
-
-  let dayDifference = today.getDate() - birthDate.getDate();
-  let monthDifference = today.getMonth() - birthDate.getMonth();
-  let ageInYears = today.getFullYear() - birthDate.getFullYear();
-
-  if (monthDifference < 0 || (!monthDifference && dayDifference < 0)) {
-    ageInYears--;
-  }
-
-  if (monthDifference < 0) monthDifference += 12;
-
-  if (dayDifference < 0) {
-    monthDifference--;
-
-    dayDifference += new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      0
-    ).getDate();
-
-    if (monthDifference < 0) {
-      ageInYears--;
-      monthDifference += 12;
-    }
-  }
-
-  return { ageInYears, ageInMonths: monthDifference, ageInDays: dayDifference };
 }
 
 inputDay.addEventListener(
@@ -102,54 +70,55 @@ inputYear.addEventListener(
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  let invalid = false;
+
   let dob = new Date(inputYear.value, inputMonth.value - 1, inputDay.value);
+  let invalid = false;
 
   if (!inputDay.value) {
+    validationDayEmpty.classList.remove('d-none');
+    validationDay.classList.add('d-none');
     labelDay.classList.add('invalid');
     inputDay.classList.add('invalid');
-    validationDay.classList.add('d-none');
-    validationDayEmpty.classList.remove('d-none');
     invalid = true;
   }
 
   if (inputDay.value > 31 || inputDay.value < 0) {
+    validationDayEmpty.classList.add('d-none');
+    validationDay.classList.remove('d-none');
     labelDay.classList.add('invalid');
     inputDay.classList.add('invalid');
-    validationDay.classList.remove('d-none');
-    validationDayEmpty.classList.add('d-none');
     invalid = true;
   }
 
   if (!inputMonth.value) {
+    validationMonthEmpty.classList.remove('d-none');
+    validationMonth.classList.add('d-none');
     labelMonth.classList.add('invalid');
     inputMonth.classList.add('invalid');
-    validationMonth.classList.add('d-none');
-    validationMonthEmpty.classList.remove('d-none');
     invalid = true;
   }
 
   if (inputMonth.value > 12 || inputMonth.value < 0) {
+    validationMonthEmpty.classList.add('d-none');
+    validationMonth.classList.remove('d-none');
     labelMonth.classList.add('invalid');
     inputMonth.classList.add('invalid');
-    validationMonth.classList.remove('d-none');
-    validationMonthEmpty.classList.add('d-none');
     invalid = true;
   }
 
   if (!inputYear.value) {
+    validationYearEmpty.classList.remove('d-none');
+    validationYear.classList.add('d-none');
     labelYear.classList.add('invalid');
     inputYear.classList.add('invalid');
-    validationYear.classList.add('d-none');
-    validationYearEmpty.classList.remove('d-none');
     invalid = true;
   }
 
   if (inputYear.value > new Date().getFullYear() || inputYear.value < 0) {
-    labelYear.classList.add('invalid');
-    inputYear.classList.add('invalid');
     validationYear.classList.remove('d-none');
     validationYearEmpty.classList.add('d-none');
+    labelYear.classList.add('invalid');
+    inputYear.classList.add('invalid');
     invalid = true;
   }
 
@@ -160,17 +129,8 @@ form.addEventListener('submit', function (event) {
 
   if (invalid) return;
 
-  dob = new Date();
-  dob.setHours(0);
-  dob.setMinutes(0);
-  dob.setSeconds(0);
-  dob.setMilliseconds(0);
-  dob.setDate(inputDay.value);
-  dob.setFullYear(inputYear.value);
-  dob.setMonth(inputMonth.value - 1);
-
-  const age = calculateAge(dob);
-  displayDay.textContent = age.ageInDays;
-  displayMonth.textContent = age.ageInMonths;
-  displayYear.textContent = age.ageInYears;
+  const age = dateFns.intervalToDuration({ start: dob, end: new Date() });
+  displayMonth.textContent = age.months || 0;
+  displayYear.textContent = age.years || 0;
+  displayDay.textContent = age.days || 0;
 });
